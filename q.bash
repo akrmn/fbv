3344da6727fec2db66f948777c88e653fc45e2bf
Moisés Ackerman <akrmn@users.noreply.github.com>
2016-06-12 20:02:09 -0400

    fixed q.bash to use original database

--- q.bash
+++ q.bash
@@ -5,12 +5,10 @@ echo q"$1"
 cd q"$1"
 
 echo "Consulta original"
-echo "  Borrando índices"
-psql -qAt -f dropIndices"$1".sql > /dev/null
 echo "  Limpiando cache"
 sudo ../clearcache.bash > /dev/null
 echo "  Ejecutando consulta original"
-psql -qAt -f execute"$1"original.sql > results/analysis"$1"original.yml
+psql -d original -qAt -f execute"$1"original.sql > ../results/analysis"$1"original.yml
 echo "Consulta original completa"
 
 echo
@@ -21,7 +19,7 @@ psql -qAt -f indices"$1".sql > /dev/null
 echo "  Limpiando cache"
 sudo ../clearcache.bash > /dev/null
 echo "  Ejecutando consulta mejorada"
-psql -qAt -f execute"$1"improved.sql > results/analysis"$1"improved.yml
+psql -qAt -f execute"$1"improved.sql > ../results/analysis"$1"improved.yml
 echo "Consulta mejorada completa"
 
 echo
