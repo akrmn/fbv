aefb337c4c005a64d085cedd19f5d8d5de597b51
Moisés Ackerman <akrmn@users.noreply.github.com>
2016-06-13 23:14:03 -0400

    removed bad query 23

--- q23/execute23improved2.sql
+++
@@ -1,4 +0,0 @@
-\i q23improved2.sql
-
-explain (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT YAML)
-execute q23improved2 ('13','31','23','29','30','18','17');
--- q23/execute23textimproved2.sql
+++
@@ -1,4 +0,0 @@
-\i q23improved2.sql
-
-explain (ANALYZE, COSTS, VERBOSE, BUFFERS)
-execute q23improved2 ('13','31','23','29','30','18','17');
