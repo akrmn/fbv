034f07c351bcecc4ea99bd78e6b9d19e4f0c2aac
Moisés Ackerman <akrmn@users.noreply.github.com>
2016-06-15 14:11:12 -0400

    cleaning up queries

--- q23/q23improved.sql
+++ q23/q23improved.sql
@@ -28,32 +28,3 @@ group by
   cntrycode
 order by
   cntrycode;
-
-/*
-select
-  substring(c_phone from 1 for 2) as cntrycode,
-  count(1)                        as numcust,
-  sum(c_acctbal)                  as totacctbal
-from
-  customer
-where substring(c_phone from 1 for 2) in ('13', '31', '23', '29', '30', '18', '17')
-  and c_acctbal > (
-    select
-      avg(c_acctbal)
-    from
-      customer
-    where substring(c_phone from 1 for 2) in ('13', '31', '23', '29', '30', '18', '17')
-      and c_acctbal > 0.00
-  )
-  and not exists (
-    select 1
-    from
-      orders
-    where
-      o_custkey = c_custkey
-  )
-group by
-  cntrycode
-order by
-  cntrycode;
-*/
